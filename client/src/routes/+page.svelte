20630017238170efbcefbb49fddd63c9b7a2cd0d
ayt <afriansyoga@gmail.com>
2025-12-08 11:28:44 +0700

    setup FE

--- client/src/routes/+page.svelte
+++ client/src/routes/+page.svelte
@@ -1,2 +1,16 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+  import { onMount } from "svelte";
+
+
+  async function getTicketParking() {
+    
+  }
+
+  onMount(() => {
+    // getTicketParking();
+  })
+</script>
+
+<div class="">
+
+</div>
